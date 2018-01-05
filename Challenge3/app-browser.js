76ae186b906e72fbef94e2dbeda2d29495e30fd1
Rob Shelley <rob@pixelinspiration.net>
2018-01-05 16:01:52 +0000

    Clean up and remove lodash to nark Jimi off ;)

--- Challenge3/app-browser.js
+++ Challenge3/app-browser.js
@@ -1,40 +1,24 @@
-$(function(){
+$(function () {
 	//load the json
-	$.getJSON('maze1.json', function( data ){
-		//convert the data if required
-		//data = Array.isArray( data ) ? data : JSON.parse( data );
-		
-		function refreshMaze(){
-			var maze = model.maze();
-			model.maze( [] );
-			model.maze( maze );
-		}
-
-		//display the problem
-		var model = new AppModel({
-			maze : data
-		});
-
-		ko.applyBindings( model );
-		//calculate the solution
-
-		//update the display
-		refreshMaze();
+	$.getJSON('maze1.json', function (data) {
 
 		var easystar = new EasyStar.js();
-
-		easystar.setGrid(model.maze());
+		easystar.setGrid(data);
 		easystar.setAcceptableTiles(['c']);
-		easystar.findPath(7, 0, 7, 14, function( path ) {
+		easystar.findPath(7, 0, 7, 14, function (path) {
 			if (path === null) {
 				alert("Path was not found.");
 			} else {
-				console.log(path)
-				let m = model.maze()
-				_.map(path, (point) => {
-					m[point.y][point.x] = 'r'
+				path.map((point) => {
+					data[point.y][point.x] = 'r'
 				})
-				refreshMaze();
+
+				//create model with modded data
+				var model = new AppModel({
+					maze: data
+				});
+
+				ko.applyBindings(model);
 			}
 		});
 		easystar.calculate()
@@ -43,10 +27,10 @@ $(function(){
 });
 
 
-function AppModel( options ){
-	this.maze = ko.observableArray( options.maze || [] );
+function AppModel(options) {
+	this.maze = ko.observableArray(options.maze || []);
 
-	this.onClick = function(type, evt){
+	this.onClick = function (type, evt) {
 		console.log(type, evt)
 	}
 }
